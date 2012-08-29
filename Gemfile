dff24ed0910eaeaeae7f412b78f9ae61f4f4551d
david <davidqhr@gmail.com>
2012-08-29 12:24:40 +0800

    init

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'http://ruby.taobao.org'
+
+gem 'rails', '3.2.7'
+
+gem 'sqlite3'
+gem 'devise'
+#gem "redcarpet"
+#gem "wmd-rails"
+gem "rails-i18n"
+gem 'jquery-rails'
+
+group :development, :test do
+  gem 'factory_girl_rails'
+end
+
+group :assets do
+  gem 'coffee-rails'
+  gem 'uglifier'
+  gem 'therubyracer'
+  gem 'sass-rails'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zone::Application.config.secret_token = '47ffe3bda74c86381895e7127a1970f8432348a647634994e0f633fa70b4dad66a97da02089f73cd990fcaa0cf918315c0c5ba3edf6dfd01f41a592e1870debb'
